d61f00f791917e01c6ec4598b04d33d7d964bb4e
Ivan <ivan.anisimov@coldsnipe.com>
2015-10-03 22:53:29 +0300

    Acceptence tests added

--- src/test/scala/pw/anisimov/adverto/AcceptanceSpec.scala
+++ src/test/scala/pw/anisimov/adverto/AcceptanceSpec.scala
@@ -1,37 +1,48 @@
 package pw.anisimov.adverto
 
+import java.time.{OffsetDateTime, ZoneOffset}
 import java.util.UUID
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
 import akka.testkit.TestProbe
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FeatureSpec, GivenWhenThen}
-import pw.anisimov.adverto.api.ApiManagerActor
 import pw.anisimov.adverto.api.ApiManagerActor.GetBinding
+import pw.anisimov.adverto.api.{AdvertoJsonProtocol, ApiManagerActor, NewCarAdvert}
 import pw.anisimov.adverto.data.CarAdvertsPersitor
 import pw.anisimov.adverto.data.CarAdvertsPersitor.{DeleteAdvert, GetAdverts}
-import pw.anisimov.adverto.data.model.CarAdvert
+import pw.anisimov.adverto.data.model.{CarAdvert, Diesel, Gasoline}
+import spray.json._
+import HttpMethods._
+import StatusCodes._
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
-class AcceptanceSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll with BeforeAndAfter {
+class AcceptanceSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll with BeforeAndAfter with AdvertoJsonProtocol {
   val config = ConfigFactory.load()
 
   implicit val system = ActorSystem()
+  implicit val timeout = 1.second
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
 
   val dataActor = system.actorOf(CarAdvertsPersitor.props(UUID.randomUUID().toString))
   val apiManager = system.actorOf(ApiManagerActor.props(config.getString("adverto.hostname"), config.getInt("adverto.port"),
     dataActor))
 
-  var serviceUri: Http.ServerBinding = _
+  var serviceUri: String = _
 
   override def beforeAll(): Unit = {
     val probe = TestProbe()
     probe.expectNoMsg(2.seconds)
     probe.send(apiManager, GetBinding)
-    serviceUri = probe.expectMsgClass(classOf[Option[Http.ServerBinding]]).get
+    val binding = probe.expectMsgClass(classOf[Option[Http.ServerBinding]]).get
+    serviceUri = s"http://${binding.localAddress.getHostName}:${binding.localAddress.getPort}"
   }
 
   override def afterAll(): Unit = {
@@ -50,44 +61,105 @@ class AcceptanceSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterA
     }
   }
 
+  def createAdvert(advert: CarAdvert): Unit = {
+    val probe = TestProbe()
+    probe.send(dataActor, advert)
+    probe.expectMsg(advert)
+  }
+
+  val VolvoAdvert = CarAdvert(UUID.randomUUID(), "Volvo S60", Gasoline, 15000, `new` = true)
+  val AudiAdvert = CarAdvert(UUID.randomUUID(), "Audi A4", Gasoline, 17000, `new` = true)
+  val MercedesAdvert = CarAdvert(UUID.randomUUID(), "Mercedes E320", Gasoline, 5000, `new` = false, Some(120000),
+    Some(OffsetDateTime.of(2003, 8, 10, 0, 0, 0, 0, ZoneOffset.UTC)))
+
   feature("Car Service should provide REST api") {
     info("As a user of the Car Advert Service ")
     info("I want be able to interact with REST API")
 
     scenario("have functionality to return list of all car adverts") {
-      fail()
+      Given("collection of adverts")
+      createAdvert(VolvoAdvert)
+      createAdvert(MercedesAdvert)
+      createAdvert(AudiAdvert)
+      When("get message sent to REST API")
+      val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = s"$serviceUri/advert")).mapTo[HttpResponse]
+      Then("answer should contain all adverts")
+      assert(Await.result(Unmarshal(Await.result(response, timeout).entity).to[Array[CarAdvert]], timeout).toSet ===
+        Set(VolvoAdvert, AudiAdvert, MercedesAdvert))
     }
 
     scenario("have functionality to return data for single car advert by id") {
-      fail()
+      Given("single advert")
+      createAdvert(VolvoAdvert)
+      When("get message sent to REST API with UUID")
+      val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri =
+        s"$serviceUri/advert/${VolvoAdvert.id.toString}")).mapTo[HttpResponse]
+      Then("answer should contain direct advert")
+      assert(Await.result(Unmarshal(Await.result(response, timeout).entity).to[CarAdvert], timeout) === VolvoAdvert)
     }
 
     scenario("have functionality to add car advert") {
-      fail()
+      Given("no adverts")
+      When("POST message sent to REST API")
+      val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = s"$serviceUri/advert", method = POST)
+      .withEntity(HttpEntity(ContentTypes.`application/json`, NewCarAdvert("Car", Diesel, 1000, `new` = true).toJson.toString()))).mapTo[HttpResponse]
+      Then("answer should contain 201 status")
+      assert(Await.result(response, timeout).status === Created)
+      And("GET Request should find new record")
+      val getResponse: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = s"$serviceUri/advert")).mapTo[HttpResponse]
+      assert(Await.result(Unmarshal(Await.result(getResponse, timeout).entity).to[Array[CarAdvert]], timeout).length === 1)
     }
 
     scenario("have functionality to modify car advert by id") {
-      fail()
+      Given("single advert")
+      createAdvert(MercedesAdvert)
+      When("PUT message sent to REST API with UUID")
+      val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+        uri = s"$serviceUri/advert/${MercedesAdvert.id.toString}", method = PUT)
+        .withEntity(HttpEntity(ContentTypes.`application/json`, CarAdvert(MercedesAdvert.id,
+        "Mercedes Car NEW!", Gasoline, 33000, `new` = true).toJson.toString()))).mapTo[HttpResponse]
+      Then("answer should contain 204 status")
+      assert(Await.result(response, timeout).status === NoContent)
+      And("GET Request should find modified record")
+      val getResponse: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri =
+        s"$serviceUri/advert/${MercedesAdvert.id.toString}")).mapTo[HttpResponse]
+      assert(Await.result(Unmarshal(Await.result(getResponse, timeout).entity).to[CarAdvert], timeout).title ===
+        "Mercedes Car NEW!")
     }
 
     scenario("have functionality to delete car advert by id") {
-      fail()
+      Given("single advert")
+      createAdvert(AudiAdvert)
+      When("DELETE message sent to REST API with UUID")
+      val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+        uri = s"$serviceUri/advert/${AudiAdvert.id.toString}", method = DELETE)).mapTo[HttpResponse]
+      Then("answer should contain 204 status")
+      assert(Await.result(response, timeout).status === NoContent)
+      And("GET Request should not find new record")
+      val getResponse: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri =
+        s"$serviceUri/advert/${AudiAdvert.id.toString}")).mapTo[HttpResponse]
+      assert(Await.result(getResponse, timeout).status === OK)
     }
 
     scenario("have validation by fields and fields used only for used cars") {
-      fail()
-    }
-
-    scenario("accept and return data in JSON format") {
-      fail()
-    }
-
-    scenario("Service should be able to handle CORS requests from any domain") {
-      fail()
+      Given("no adverts")
+      When("POST message sent to REST API with wrong values")
+      val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = s"$serviceUri/advert", method = POST)
+        .withEntity(HttpEntity(ContentTypes.`application/json`, NewCarAdvert("Car", Diesel, 1000, `new` = false).toJson.toString()))).mapTo[HttpResponse]
+      Then("answer should contain 400 status")
+      assert(Await.result(response, timeout).status === BadRequest)
     }
 
     scenario("sorting by any field specified by query parameter, default sorting - by **id**") {
-      fail()
+      Given("collection of adverts")
+      createAdvert(VolvoAdvert)
+      createAdvert(MercedesAdvert)
+      createAdvert(AudiAdvert)
+      When("get message sent to REST API with sorting by title")
+      val response: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = s"$serviceUri/advert?sort=title")).mapTo[HttpResponse]
+      Then("answer should contain all adverts")
+      assert(Await.result(Unmarshal(Await.result(response, timeout).entity).to[Array[CarAdvert]], timeout).head.title ===
+        AudiAdvert.title)
     }
   }
 }
